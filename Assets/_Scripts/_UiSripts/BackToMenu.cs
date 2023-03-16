54090b788719192dfea8a93981953108b5a4cd9b
KarolMirowski <karolpoczta@protonmail.com>
2023-03-16 17:18:58 +0100

    Menu References
    Main menu, settings and credits, references and scripts ready for first release.

--- Assets/_Scripts/_UiSripts/BackToMenu.cs
+++ Assets/_Scripts/_UiSripts/BackToMenu.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
 
-public class BackToMenu : MonoBehaviour
+public class BackToMenu : MonoBehaviour, IPointerDownHandler
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private AudioClip _audioClip;
+    [SerializeField] private GameObject _menuPanel;
+    [SerializeField] private GameObject _settingsPanel;
+    [SerializeField] private GameObject _creditsPanel;
 
-    // Update is called once per frame
-    void Update()
+    public void OnPointerDown(PointerEventData eventData)
     {
+        _audioSource.PlayOneShot(_audioClip);
         
+        if(_menuPanel.activeSelf == false)
+            _menuPanel.SetActive(true);
+        
+        if(_settingsPanel.activeSelf == true)
+            _settingsPanel.SetActive(false);
+        
+        if(_creditsPanel.activeSelf == true)
+            _creditsPanel.SetActive(false);
+    
     }
 }
