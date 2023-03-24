4d262c1658be673ec63cc2dc378eec5b2fa70771
KarolMirowski <karolpoczta@protonmail.com>
2023-03-24 16:06:32 +0100

    Menu Bots last fix before alpha

--- Assets/_Scripts/Gravity/PlanetGrav.cs
+++ Assets/_Scripts/Gravity/PlanetGrav.cs
@@ -4,11 +4,24 @@ using UnityEngine;
 
 public class PlanetGrav : MonoBehaviour {
 
-    public float gravity = -12;
+    public float gravity = -10f;
+    public Transform sphereTransform;
+    private Transform _playerTransform;
+    
+    void Start()
+    {
+        GetComponent<Rigidbody>().useGravity = false;
+        _playerTransform = transform;
+    }
+
+    void FixedUpdate()
+    {
+        Attract(_playerTransform);
+    }
 
     public void Attract(Transform playerTransform)
     {
-        Vector3 gravityUp = (playerTransform.position - transform.position).normalized;
+        Vector3 gravityUp = (playerTransform.position - sphereTransform.position).normalized;
         Vector3 localUp = playerTransform.up;
 
         playerTransform.GetComponent<Rigidbody>().AddForce(gravityUp * gravity);
