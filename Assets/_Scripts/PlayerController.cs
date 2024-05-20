f20bbcb3db2fd4a3ac1c36dea3f79d5aba10e125
KarolMirowski <109529219+KarolMirowski@users.noreply.github.com>
2024-05-20 15:26:25 +0200

    New Input System Implementation
    New unity input system has been implemented properly.

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -1,20 +1,14 @@
 using static UnityEngine.Debug;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-//using UnityEngine.InputSystem;
-//using System;
-
 using TMPro;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
-using System;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.OnScreen;
 
 public class PlayerController : MonoBehaviour
 {
-
-
-
     public float speed = 3f;
     public float rotationSpeed = 200f;
     public float horizontal = 0.3f;
@@ -22,11 +16,20 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Joystick joystick;
     [SerializeField] private TextMeshProUGUI text;
     [SerializeField] private GameSettings gameSettings;
-    private TrailRenderer tr;
+    [SerializeField] private PlayerInput _playerInput;
     bool IsBonus1Active = false;
     int bonus1Reps = 0;
     public GameObject turnLeftButton;
     public GameObject turnRightButton;
+    public InputAction turnAction;
+    private float tempHorizontal;
+    private TrailRenderer tr;
+    
+    void OnValidate()
+    {
+        _playerInput = GetComponent<PlayerInput>();
+    }
+
     private void Start()
     {
         if (GetComponentInChildren<Camera>() != null)
@@ -44,88 +47,58 @@ public class PlayerController : MonoBehaviour
         else
             speed = gameSettings.pTwoSpeed;
 
+        //Rejestracja eventów starego systemu inpput
         turnLeftButton.GetComponent<Button>().onClick.AddListener(OnTurnLeftButtonPressed);
         turnRightButton.GetComponent<Button>().onClick.AddListener(OnTurnRightButtonPressed);
-    
+        //var gamepad = Game
+        
     }
+
     void Update()
     {
-        
+        //tempHorizontal = _playerInput.actions["Move"].ReadValue<Vector2>().x;
+        //Debug.Log($"Horizontal Input: {tempHorizontal}");
+        if (turnAction.WasPressedThisFrame()){
+            TurnLeft();
+            print("TURN ACTION DZIAŁĄ");
+        }
     }
 
     void FixedUpdate()
     {
-        ConstantMoveForward();                          
-        //JoystickRotate();                 
-        //Rotate90Degrees();                  
-        //RotateCamera90Degrees();                    
-    }
-    void LateUpdate()
-    {
-        Rotate90Degrees();
-        RotateCamera90Degrees();
+        ConstantMoveForward();
+        //JustTurn();
     }
 
-    void Rotate90Degrees()
+    public void JustTurn()
     {
-
-        //Constant move forward function call here to check eficancy.
-
-        //Moving forward a littl
-        //Turn left 90 degrees
-        if (Input.GetKeyDown(KeyCode.A) )
+        float threshold = 0.1f;
+        if (tempHorizontal < -threshold)
         {
             TurnLeft();
-            //transform.position += transform.forward * 0.5f;
         }
-
-        //Turn right 90 degrees
-        if (Input.GetKeyDown(KeyCode.D))
+        else if (tempHorizontal > threshold)
         {
             TurnRight();
-            //transform.position += transform.forward * 0.5f;
         }
-    }
-    void RotateCamera90Degrees()
-    {
-        //Turn left 90 degrees
-        if (Input.GetKeyDown(KeyCode.A))
-            camera.transform.Rotate(Vector3.forward * -90f); 
-        
-        //Turn right 90 degrees
-        if (Input.GetKeyDown(KeyCode.D))
-            camera.transform.Rotate(Vector3.forward * 90f);
-        
-
-    }
-    void JoystickRotate()
-    {
-        horizontal = -joystick.Vertical;
-        transform.Rotate(Vector3.up * horizontal * rotationSpeed);
-
-        if (camera != null)
-            camera.transform.Rotate(Vector3.forward * horizontal * rotationSpeed);
     }
 
     void ConstantMoveForward()
     {
         transform.Translate(Vector3.forward * speed);
-
     }
 
     public void TrailTactBonus()
     {
         Debug.Log("TrailTactBonus przed petla");
-        if (IsBonus1Active == false)
+        if (!IsBonus1Active)
         {
             StartCoroutine(TrailTact());
             IsBonus1Active = true;
             Debug.Log("Trailtactbonus petla");
-
         }
-
-
     }
+
     IEnumerator TrailTact()
     {
         if (bonus1Reps < 12)
@@ -136,7 +109,6 @@ public class PlayerController : MonoBehaviour
             yield return new WaitForSeconds(0.5f);
             bonus1Reps += 1;
             StartCoroutine(TrailTact());
-
         }
         else
         {
@@ -144,47 +116,69 @@ public class PlayerController : MonoBehaviour
             StopCoroutine(TrailTact());
             IsBonus1Active = false;
         }
-
-
-
     }
+
     public void TextSpeed()
     {
         text.text = speed.ToString();
     }
-    // Metody obsługi przycisków
+
     public void OnTurnLeftButtonPressed()
     {
         TurnLeft();
+        print("moved left!");
     }
 
     public void OnTurnRightButtonPressed()
     {
         TurnRight();
+        print("A teraz nam poszło right");
     }
 
     private void TurnLeft()
     {
         transform.Rotate(Vector3.up * -90f);
-        //transform.position += transform.forward * 0.5f;
+        TurnCameraRight90();
+        if (!IsTurning())
+        {
+        }
     }
 
     private void TurnRight()
     {
-        transform.Rotate(Vector3.up * 90f);
-        TurnCameraLeft90();
-        //transform.position += transform.forward * 0.5f;
+        if (!IsTurning())
+        {
+            transform.Rotate(Vector3.up * 90f);
+            TurnCameraLeft90();
+        }
+    }
+    public void OnTurnLeft(InputAction.CallbackContext context)
+    {
+        if (context.performed && context.control.name == "a")
+        {
+            TurnLeft();
+        }
     }
 
-
-    void TurnCameraLeft90(){
-        camera.transform.Rotate(Vector3.forward * -90f); 
+    public void OnTurnRight(InputAction.CallbackContext context)
+    {
+        if (context.performed && context.control.name == "d")
+        {
+            TurnRight();
+        }
     }
-    
-    void TurnCameraRight90(){
-        camera.transform.Rotate(Vector3.forward * 90f); 
+    private bool IsTurning()
+    {
+        return tempHorizontal != 0;
     }
 
+    void TurnCameraLeft90()
+    {
+        camera.transform.Rotate(Vector3.forward * 90f);
+    }
+
+    void TurnCameraRight90()
+    {
+        camera.transform.Rotate(Vector3.forward * -90f);
+    }
 }
-
-
