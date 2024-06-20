996e1dd13fb2ab09ad7d963b920d94589f59643d
KarolMirowski <109529219+KarolMirowski@users.noreply.github.com>
2024-06-20 13:58:39 +0200

    Fixed Collisions
    Fixed Collisions with Trailons.

--- Assets/_Scripts/TrailMesh.cs
+++ Assets/_Scripts/TrailMesh.cs
@@ -8,47 +8,64 @@ using Unity.Burst;
 
 public class TrailMesh : MonoBehaviour
 {
-    
 
-    
-    
+
+
+
     public TrailRenderer tr;
 
     private Mesh mesh;
-    
+    private Camera camera;
+
 
     private bool collisionBool;
     public void trInit()
     {
-        
+
         //GameObject player = GameObject.FindGameObjectWithTag("Player");
-        
-        
-        
+        //cos
+
+
     }
     public void Awake()
     {
         mesh = new Mesh();
         mesh.MarkDynamic();
         mesh.Optimize();
+        
+    }
+
+    void Start()
+    {
+        if (gameObject.name == "TrailonOne")
+            camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        if (gameObject.name == "TrailonTwo")
+            camera = GameObject.FindGameObjectWithTag("MainCamera2").GetComponent<Camera>();
+        print(camera.gameObject.GetComponentInParent<PlayerController>().gameObject.name);
+        
+    }
+
+    public void OnValidate()
+    {
+
     }
 
     void FixedUpdate()
     {
-        tr.BakeMesh(mesh, useTransform: true);
+        tr.BakeMesh(mesh, camera, useTransform: true);
 
         GetComponent<MeshFilter>().mesh = mesh;
-        if(mesh.vertexCount > 5)
+        if (mesh.vertexCount > 5)
             GetComponent<MeshCollider>().sharedMesh = mesh;
-    
+
     }
-        
 
 
 
-    
-    
-  
+
+
+
+
 
 
 }
